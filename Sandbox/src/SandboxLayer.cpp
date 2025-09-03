f23d4b19adfedd4ac0eb8ec1a1a07cfba68453d5
ThatTanishqTak <tanishqtak3@gmail.com>
2025-09-03 15:38:11 +0100

    Addded some basic controles

--- Sandbox/src/SandboxLayer.cpp
+++ Sandbox/src/SandboxLayer.cpp
@@ -6,7 +6,7 @@
 
 SandboxLayer::SandboxLayer(Engine::Renderer* renderer) : Engine::Layer("SandboxLayer"), m_Renderer(renderer)
 {
-
+    m_Sphere = Engine::Sphere(m_ObjectPosition, 1.0f);
 }
 
 void SandboxLayer::OnAttach()
@@ -21,19 +21,35 @@ void SandboxLayer::OnDetach()
 
 void SandboxLayer::OnUpdate(float deltaTime)
 {
-	UpdateCamera(m_Renderer->GetCamera(), CAMERA_FREE);
+    ApplySettings();
+    UpdateCamera(m_Renderer->GetCamera(), CAMERA_FREE);
 }
 
 void SandboxLayer::OnImGuiRender()
 {
-	rlImGuiBegin();
+    rlImGuiBegin();
 
-	ImGui::Text("TEST");
+    ImGui::SliderFloat("Camera FOV", &m_CameraFov, 1.0f, 120.0f);
+    ImGui::SliderFloat3("Object Position", &m_ObjectPosition.x, -10.0f, 10.0f);
 
-	rlImGuiEnd();
+    float l_Color[3] = { m_MaterialColor.r / 255.0f, m_MaterialColor.g / 255.0f, m_MaterialColor.b / 255.0f };
+    if (ImGui::ColorEdit3("Material Color", l_Color))
+    {
+        m_MaterialColor.r = static_cast<unsigned char>(l_Color[0] * 255.0f);
+        m_MaterialColor.g = static_cast<unsigned char>(l_Color[1] * 255.0f);
+        m_MaterialColor.b = static_cast<unsigned char>(l_Color[2] * 255.0f);
+    }
+
+    rlImGuiEnd();
 }
 
 void SandboxLayer::OnSceneRender()
 {
-	DrawTexture(m_Renderer->GetFrameTexture(), 0, 0, WHITE);
+    DrawTexture(m_Renderer->GetFrameTexture(), 0, 0, WHITE);
+}
+
+void SandboxLayer::ApplySettings()
+{
+    m_Renderer->GetCamera()->fovy = m_CameraFov;
+    m_Sphere = Engine::Sphere(m_ObjectPosition, 1.0f);
 }
--- Sandbox/src/SandboxLayer.h
+++ Sandbox/src/SandboxLayer.h
@@ -3,21 +3,30 @@
 #include "Core/Layer.h"
 
 #include "Renderer/Renderer.h"
+#include "Tracer/Sphere.h"
+
+#include <raylib.h>
 
 class SandboxLayer : public Engine::Layer
 {
 public:
-	SandboxLayer(Engine::Renderer* renderer);
-	virtual ~SandboxLayer() = default;
+    SandboxLayer(Engine::Renderer* renderer);
+    virtual ~SandboxLayer() = default;
 
-	virtual void OnAttach() override;
-	virtual void OnDetach() override;
+    virtual void OnAttach() override;
+    virtual void OnDetach() override;
 
-	virtual void OnUpdate(float deltaTime) override;
-	virtual void OnImGuiRender() override;
-	virtual void OnSceneRender() override;
+    virtual void OnUpdate(float deltaTime) override;
+    virtual void OnImGuiRender() override;
+    virtual void OnSceneRender() override;
 
 private:
-	Engine::Renderer* m_Renderer = nullptr;
+    void ApplySettings();
+
+    Engine::Renderer* m_Renderer = nullptr;
+    float m_CameraFov = 45.0f;
+    Vector3 m_ObjectPosition{ 0.0f, 0.0f, 0.0f };
+    Color m_MaterialColor{ 255, 255, 255, 255 };
+    Engine::Sphere m_Sphere;
 
 };
